c49913e3c17ca7f02800055d5f1851ca596af2af
armanirmr <armanirmr@gmail.com>
2022-02-28 17:11:36 -0600

    Slack Dialog has been implemented on our bot
    - Renamed bot.rb file as chambas_bot.rb
    - Created ChambasBot class
    - Created bot_dialog method to invoke the dialog
    - Created generate_dialog method in commands_helper to return the dialog
    - Modified create method in commands_controller:
    	- It now creates a ChambasBot instance, generates a dialog and calls it
    	- We still need to receive the data from the dialog and make use of it
    
    Co-authored-by: BrandonVG <bvadillo0@ucol.mx>
    Co-authored-by: diegoOG09 <xdiegoemmax@outlook.com>
    Co-authored-by: YxMARCKxY <mramirez73@ucol.mx>
    Co-authored-by: rene3255 <ranchoeltecal@gmail.com>

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,103 +1,32 @@
-require 'slack-ruby-client'
+require_relative '../slackbot/chambas_bot'
 require 'httparty'
 
 
 class CommandsController < ApplicationController
   skip_before_action :verify_authenticity_token
+	
 
   def create
+		bot = ChambasBot.new
+		dialog = helpers.generate_dialog
+		bot.bot_dialog(dialog.to_json, params[:trigger_id])
+		render status: 200, json: ''
+		# HTTParty.post(params[:response_url], { body: message.to_json, headers: {
+		# 	"Content-Type" => "application/json"
+		#  }
+		# })
+			
+	end
 
-    #{r}eturn render json: {}, status: 403 unless valid_slack_token?
-    #{}CommandWorker.perform_async(command_params.to_h)
-    render json: { response_type: "in_channel" }, status: :created
-    p "Bingo"
-=begin
-    Slack.configure do |config|
-      config.token = ENV['SLACK_API_TOKEN']
-      raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
-    end
-    client = Slack::Web::Client.new
-    client.auth_test
-    client.chat_postMessage(channel: '#general', text: 'respuesta de prueba')
-=end    
-message = {
-	"blocks": [
-		{
-			"type": "input",
-			"element": {
-				"type": "plain_text_input",
-				"action_id": "plain_text_input-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Name",
-				"emoji": true
-			}
-		},
-		{
-			"type": "input",
-			"element": {
-				"type": "plain_text_input",
-				"action_id": "plain_text_input-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Skills",
-				"emoji": true
-			}
-		},
-		{
-			"type": "input",
-			"element": {
-				"type": "plain_text_input",
-				"action_id": "plain_text_input-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Salary",
-				"emoji": true
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "This is a section block with a button."
-			},
-			"accessory": {
-				"type": "button",
-				"text": {
-					"type": "plain_text",
-					"text": "Click Me",
-					"emoji": true
-				},
-				"value": "click_me_123",
-				"url": "https://090e-2806-102e-e-3df0-b95d-9704-e0a1-4d1e.ngrok.io/commands/dialog",
-				"action_id": "button-action"
-			}
-		}
-	]
-}
+	def dialog
+		bot = ChambasBot.new
+		data = JSON.parse(params[:payload])
+  	bot.mensaje_bueno(data["user"]["name"])
+		render status: 200, json: ''
+	end
 
-par = params[:response_url]
-p par 
-HTTParty.post(params[:response_url], { body: message.to_json, headers: {
-  "Content-Type" => "application/json"
-}
-})
-    
-end
-
-def dialog
-  p "bingo"
-end
-
-  def valid_slack_token?
-    params[:token] == ENV["SLACK_SLASH_COMMAND_TOKEN"]
-  end
-
-  def command_params
-    params.permit(:text, :token, :user_id, :response_url)
-  end
+  # def command_params
+  #   params.permit(:text, :token, :user_id, :response_url,:user,:trigger_id)
+  # end
 
 end
---
+++ app/slackbot/chambas_bot.rb
@@ -0,0 +1,30 @@
+require 'slack-ruby-client'
+
+class ChambasBot
+  def initialize()
+    Slack.configure do |config|
+      config.token = ENV['SLACK_API_TOKEN']
+      raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+    end
+    @client = Slack::Web::Client.new
+  
+    @client.auth_test
+  end
+
+  def mensaje_prueba
+    @client.chat_postMessage(channel: "general", text:'mensaje prueba')
+  end
+
+  def mensaje_bueno(user_name)
+    @client.chat_postMessage(channel: "general", text:"Chambita agregada <@#{user_name}>")
+  end
+
+  def mensaje_malo(user_id)
+    @client.chat_postMessage(channel: "general", text:'Hubo un fallo al agregar la chambita :(')
+  end
+
+  def bot_dialog(json, trigger)
+    @client.dialog_open(dialog: json, trigger_id: trigger)
+  end
+  
+end
